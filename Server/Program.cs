f3083bc21efafcd51edb226cb48536246bf9e64e
Bart Luijten <github@dibarto.nl>
2025-10-15 17:23:35 +0200

    Added Server project

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,24 +1,58 @@
-// Code hiervoor en hierna laten staan.
-// De code hieronder is toevoeging. Zorg dat je deze ook hebt.
-
-builder.Services.AddLogging(builder =>
-    builder.AddDebug()
-        .AddConsole()
-        .SetMinimumLevel(LogLevel.Information));
-
-// Prefer environment variable for connection string if present
-var connectionString = Environment.GetEnvironmentVariable("ConnectionStrings__{{JouwConnectieNaam}}")
-    ?? builder.Configuration.GetConnectionString("{{JouwConnectieNaam}}");
-Console.WriteLine($"Using connection string: {connectionString}");
-
-builder.Services.AddDbContext<MyCarsDbContext>(options =>
-  options.UseSqlServer(connectionString));
-
-var app = builder.Build();
-
-using (var scope = app.Services.CreateScope())
+namespace Server
 {
-    // Bij het opstarten zal hij de database migraties uitvoeren.
-    var db = scope.ServiceProvider.GetRequiredService<MyCarsDbContext>();
-    db.Database.Migrate();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            // Code hiervoor en hierna laten staan.
+            // De code hieronder is toevoeging. Zorg dat je deze ook hebt.
+
+            builder.Services.AddLogging(builder =>
+                builder.AddDebug()
+                    .AddConsole()
+                    .SetMinimumLevel(LogLevel.Information));
+
+            // Prefer environment variable for connection string if present
+            var connectionString = Environment.GetEnvironmentVariable("ConnectionStrings__{{JouwConnectieNaam}}")
+                ?? builder.Configuration.GetConnectionString("{{JouwConnectieNaam}}");
+            Console.WriteLine($"Using connection string: {connectionString}");
+
+            // builder.Services.AddDbContext<MyCarsDbContext>(options =>
+              // options.UseSqlServer(connectionString));
+
+            var app = builder.Build();
+
+            using (var scope = app.Services.CreateScope())
+            {
+                // Bij het opstarten zal hij de database migraties uitvoeren.
+                // var db = scope.ServiceProvider.GetRequiredService<MyCarsDbContext>();
+                // db.Database.Migrate();
+            }
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
 }
